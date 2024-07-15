3ef51588f82bb1708245161727de2d0b489f2e6b
ghwo336 <161708856+ghwo336@users.noreply.github.com>
2024-07-15 15:13:16 +0900

    [Bronze V] Title: Football Scoring, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,5 @@
+li1=list(map(int,input().split()))
+dap1=6*li1[0]+3*li1[1]+2*li1[2]+li1[3]+2*li1[4]
+li1=list(map(int,input().split()))
+dap2=6*li1[0]+3*li1[1]+2*li1[2]+li1[3]+2*li1[4]
+print(dap1,dap2)
