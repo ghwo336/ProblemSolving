571d811cfd256680429a626f6933cc269cf860f6
이호재 <161708856+ghwo336@users.noreply.github.com>
2024-11-28 00:59:57 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int dap = 0;
+    string s;
+    while(cin>>s){
+        dap+=1;
+    }
+    cout<<dap/5;
+    return 0;
+}
