d68d2ecc881d5dfb38e5cf16b1f36b64974747f5
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-01-17 00:58:17 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cin>>a>>b>>c;
+    if (a+b+c<=21){
+        cout<<1;
+      
+    } else cout<<0;
+}
