457df827dc2882c58b9ed483564e7687ee7469ac
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-07-02 18:23:08 +0900

    [Gold V] Title: 하노이 탑, Time: 432 ms, Memory: 149776 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,12 @@
+def move(n,start,end):
+    if n==1:
+        print(start,end)
+        return
+    move(n-1,start,6-start-end)
+    print(start,end)
+    move(n-1,6-start-end,end)
+
+n=int(input())
+print(2**n-1)
+if n<=20:
+    move(n,1,3)
