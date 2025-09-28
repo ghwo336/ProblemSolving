8d30e9b61dc00f935c115a8732db0494f9fba07e
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-09-28 21:39:06 +0900

    [Bronze IV] Title: Juta teekond, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.py
@@ -0,0 +1,9 @@
+a=[]
+for x in range(3):
+    k=int(input())
+    a.append(k)
+if (a[0]==1 or a[0]==3) and (a[1]==6 or a[1]==8) and (a[2]==2 or a[2]==5):
+    print('JAH')
+else:
+    print('EI')
+    
