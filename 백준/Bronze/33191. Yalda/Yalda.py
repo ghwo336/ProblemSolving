a538c95687ddba8120a447761d195fa21da430c8
이호재 <161708856+ghwo336@users.noreply.github.com>
2025-12-04 14:52:11 +0900

    [Bronze IV] Title: Yalda, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,12 @@
+fiv=int(input())
+w=int(input())
+p=int(input())
+n=int(input())
+if fiv>=w:
+    print('Watermelon')
+elif fiv>=p:
+    print('Pomegranates')
+elif fiv>=n:
+    print('Nuts')
+else:
+    print('Nothing')
